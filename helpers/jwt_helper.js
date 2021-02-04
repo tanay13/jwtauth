31b580828a71db3e75f263e02275b8e356c1f0c9
Tanay Raj <tanay.raj76@gmail.com>
2021-02-05 00:05:06 +0530

    secrets added

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -10,7 +10,7 @@ module.exports = {
             const payload = {
                 
             }
-            const secret = "super secret"
+            const secret = process.env.ACCESS_TOKEN_SECRET
             const options = {
                 expiresIn : "1h",
                 issuer : "xyz.com",
